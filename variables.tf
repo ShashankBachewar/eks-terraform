d3947c029598710c3381ab7cec65d035ae927c08
ShashankBachewar <127659325+ShashankBachewar@users.noreply.github.com>
2025-06-29 19:38:51 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region"         { type = string }
+variable "cluster_name"       { type = string }
+variable "vpc_id"             { type = string }
+variable "private_subnet_ids" { type = list(string) }
